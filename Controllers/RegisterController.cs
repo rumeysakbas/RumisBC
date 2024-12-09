a2fff681c7409cdaafdf7ec640d4278ecb2c6f5f
Rümeysa Akbaş <rumeysakbas28@gmail.com>
2024-12-09 21:34:29 +0300

    register added

--- Controllers/RegisterController.cs
+++ Controllers/RegisterController.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using RumisBC.Models;
 
 namespace RumisBC.Controllers
-{
+{using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
     public class RegisterController : Controller
     {
+
+        private Context context = new Context();
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Customer customer)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View("Index", customer);
+            }
+
+            context.Customers.Add(customer);
+            context.SaveChanges();
+
+            return RedirectToAction("Index", "Login");
         }
     }
 }
